457126e96b96c9802da7cfcb6526d1a4e555fd5d
Naoto Yoshimoto <naotone@floatingdots.com>
2020-02-01 22:03:13 -0500

    Update macos.sh

--- macos.sh
+++ macos.sh
@@ -10,7 +10,7 @@ osascript -e 'tell application "System Preferences" to quit'
 sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
-#while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ###############################################################################
 # General UI/UX                                                               #
@@ -616,7 +616,7 @@ defaults write com.apple.Safari SendDoNotTrackHTTPHeader -bool true
 
 # Enable Secure Keyboard Entry in Terminal.app
 # See: https://security.stackexchange.com/a/47786/8918
-# defaults write com.apple.terminal SecureKeyboardEntry -bool true
+defaults write com.apple.terminal SecureKeyboardEntry -bool true
 
 # Disable the annoying line marks
 # defaults write com.apple.Terminal ShowLineMarks -int 0
